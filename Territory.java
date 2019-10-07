e09bc961817d0a396a128202d79f74db64e9cac9
pedrogalvao <36551740+pedrogalvao@users.noreply.github.com>
2019-10-07 11:19:47 +0100

    Add files via upload

---
+++ Territory.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import WarAgent;
+
+public class Territory {
+	WarAgent player;
+	private int troops;
+	private ArrayList<Territory> frontiers;
+	
+	Territory() {
+		this.troops = 2;
+		this.frontiers = new ArrayList<Territory>(0);
+	}
+	
+	public int getTroops() {
+		return this.troops;
+	}	
+	
+	public void setTroops(int n) {
+		this.troops = n;
+	}
+
+	public void addTroops(int n) {
+		this.troops += n;
+	}
+	public void removeTroops(int n) {
+		this.troops -= n;
+		if (n <= 0) {
+			this.troops = 1;
+		}
+	}
+
+	public WarAgent getPlayer() {
+		return this.player;
+	}
+	
+	public void setPlayer(WarAgent P) {
+		this.player.removeTerritory(this);
+		P.addTerritory(this);
+	}
+	
+	public ArrayList<Territory> getFrontiers(){
+		return this.frontiers;
+	}
+	
+	public void addFrontier(Territory T) {
+		this.frontiers.add(T);
+	}
+	
+}
---
+++ WarAgent.java
@@ -0,0 +1,62 @@
+import jade.core.behaviours.Behaviour;
+
+import java.util.ArrayList;
+
+
+public class WarAgent {
+	 ArrayList<Territory> territories;
+	 
+	 WarAgent(){
+		 this.territories = new ArrayList<Territory>(0);
+	 }
+	 
+	 void addTerritory(Territory T) {
+		 T.setPlayer(this);
+		 this.territories.add(T);
+		 
+	 }	 
+	 
+	 void removeTerritory(Territory T) {
+		 T.setPlayer(this);
+		 this.territories.remove(T);
+		 
+	 }
+	 
+	 
+	 
+	 class WarBehaviour extends Behaviour {
+	 	public void action() {
+	 		return;
+		}
+	 	
+	 	public void attack(Territory T1, Territory T2, int n) {
+	 		// ataque do territorio T1 para o territorio T2 com n tropas
+	 		if (n >= T1.getTroops()) {
+	 			//movimento invalido
+	 			return;
+	 		}
+	 		else if (n < T2.getTroops()) {
+	 			T1.removeTroops(n);
+	 			T2.removeTroops(n);
+	 			return;
+	 		}
+	 		else if (n > T2.getTroops()) {
+	 			T1.removeTroops(n);
+	 			T2.setTroops(n -T2.getTroops());
+	 			T2.setPlayer(T1.getPlayer());
+	 			
+	 			return;
+	 		}
+	 		else if (n == T2.getTroops()) {
+	 			T1.removeTroops(n);
+	 			T2.setTroops(1);
+	 			return;
+	 		}	 		 		
+	 	}
+	 	
+		public boolean done() {
+			return false;
+		}
+	}
+
+}
