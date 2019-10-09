ca3e03b3cb611cae2e871ace3def79ea0aff5106
pedrogalvao <36551740+pedrogalvao@users.noreply.github.com>
2019-10-09 17:02:27 +0100

    Update Territory.java

--- Territory.java
+++ Territory.java
@@ -2,19 +2,20 @@ import java.util.ArrayList;
 import WarAgent;
 
 public class Territory {
-	WarAgent player;
-	private int troops;
-	private ArrayList<Territory> frontiers;
-	
-	Territory() {
+	public WarAgent player;
+	public int troops;
+	public ArrayList<Territory> frontiers;
+
+	public Territory() {
 		this.troops = 2;
 		this.frontiers = new ArrayList<Territory>(0);
+		this.player = null;
 	}
-	
+
 	public int getTroops() {
 		return this.troops;
 	}	
-	
+
 	public void setTroops(int n) {
 		this.troops = n;
 	}
@@ -22,6 +23,7 @@ public class Territory {
 	public void addTroops(int n) {
 		this.troops += n;
 	}
+
 	public void removeTroops(int n) {
 		this.troops -= n;
 		if (n <= 0) {
@@ -32,19 +34,18 @@ public class Territory {
 	public WarAgent getPlayer() {
 		return this.player;
 	}
-	
+
 	public void setPlayer(WarAgent P) {
-		this.player.removeTerritory(this);
-		P.addTerritory(this);
+		if (this.player != null) this.player.removeTerritory(this);
+		this.player = P;
 	}
-	
+
 	public ArrayList<Territory> getFrontiers(){
 		return this.frontiers;
 	}
-	
+
 	public void addFrontier(Territory T) {
 		this.frontiers.add(T);
 		T.frontiers.add(this);
 	}
-	
 }
