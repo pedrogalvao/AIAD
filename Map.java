d266409ad753a40b558b9d36c2fbd40477a839b3
pedrogalvao <36551740+pedrogalvao@users.noreply.github.com>
2019-10-09 17:01:47 +0100

    Update Map.java

--- Map.java
+++ Map.java
@@ -5,23 +5,42 @@ import java.util.Random;
 
 public class Map extends Agent {
 	
-	ArrayList<Territory> territories;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ArrayList<Territory> territories;
+	private ArrayList<WarAgent> agents;
 
-	Map(int n){
+	protected void setup() {
+		int n = 6;
 		this.territories = new ArrayList<Territory>(0);
 
+
+		System.out.println("Creating agents...");
+		agents=new ArrayList<WarAgent>(0);
+		for (int i = 0; i < 3; i++)	{
+			agents.add(new WarAgent());
+		}
+		System.out.println("number of agents: "+Integer.toString(agents.size()));
 		for (int i = 0; i < n; i++) {
+			System.out.println("creating territory "+Integer.toString(i));
 			this.territories.add(new Territory());
+			this.territories.get(i).setPlayer(this.agents.get(i%3));
+			System.out.println("Territory "+Integer.toString(i)+" belongs to agent "+Integer.toString(i%3));
 		}
 		for (Territory T : this.territories) {
 			Random random = new Random();
 			int front = random.nextInt(3) + 2;			 
 			for (int i = 0; i < front; i++) {
 				Random random2 = new Random();
-				int k = random2.nextInt(this.territories.size());	
+				int k = random2.nextInt(this.territories.size());
 				T.addFrontier(this.territories.get(k));
+				System.out.println("frontier "+Integer.toString(i)+" "+Integer.toString(k));
 			}
 		}
+		System.out.println("map is done");
+		
 		
 		
 	}
